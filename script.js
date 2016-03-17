e906356984407b0f8beb2e17a56a209946117eaa
erensnover <erensrocks@gmail.com>
2016-03-17 17:20:32 -0400

    all
    dsfsdfa

---
+++ script.js
@@ -0,0 +1,139 @@
+$(window).ready(function() {
+  var $sun = $('.sun');
+
+  var setupPosition = function (event) {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+
+    var sunHeight = $sun.height();
+    var sunWidth = $sun.width();
+
+    $sun.css('left', (windowWidth - sunWidth) / 2);
+    $sun.css('top', (windowHeight - sunHeight) / 2);
+  };
+
+  $(window).resize(setupPosition);
+
+  setupPosition();
+});
+$(window).keydown(function random(e){
+  var $sun = $('.sun');
+  var selector = Math.ceil(Math.random() * 6);
+
+  if(e.keyCode === 32 && selector === 1){
+    var earth = function(){
+      var earth = $('<img class="earth" style="width:50px" src="img/2.png" />');
+
+      $('.foreground').append(earth);
+
+      var sunWidth = $sun.width();
+      var t = 0;
+      var dist = 200;
+
+      setInterval(function() {
+        var width = earth.width();
+        var height = earth.height();
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+
+        earth.css('top', dist * Math.sin(t) - height / 2 + windowHeight / 2);
+        earth.css('left', dist * Math.cos(t) - width / 2 + windowWidth / 2);
+
+        t += 0.01;
+      }, 16);
+      //earthPosition();
+    };
+    earth();
+  }
+});
+
+
+// $(document).ready(function() {
+//   var $sun = $('.sun');
+//
+//   var setupPosition = function () {
+//     var windowWidth = $(window).width();
+//     var windowHeight = $(window).height();
+//
+//     var sunHeight = $sun.height();
+//     var sunWidth = $sun.width();
+//
+//     $sun.css('left', (windowWidth - sunWidth) / 2);
+//     $sun.css('top', (windowHeight - sunHeight) / 2);
+//   };
+//
+//   $(window).resize(setupPosition);
+//
+//   setupPosition();
+// });
+//
+//
+// var earth = $('<img class="earth" style="width:50px" src="img/2.png" />');
+//
+// $('.foreground').append(earth);
+//
+// var t = 0;
+// var dist = 400;
+// var drawEarth = function() {
+//   var width = earth.width();
+//   var height = earth.height();
+//   var windowWidth = $(window).width();
+//   var windowHeight = $(window).height();
+//   earth.css('top', dist * Math.sin(t) - height / 2 + windowHeight / 2);
+//   earth.css('left', dist * Math.cos(t) - width / 2 + windowWidth / 2);
+//   t += 0.01;
+// };
+//
+// var mercury = $('<img class="mercury" style="width:100px" src="img/4.png" />');
+//
+// $('.foreground1').append(mercury);
+//
+// var t2 = 0;
+// var dist2 = 500;
+// var drawMercury = function() {
+//   var width2 = mercury.width();
+//   var height2 = mercury.height();
+//   var windowWidth2 = $(window).width();
+//   var windowHeight2 = $(window).height();
+//   mercury.css('top', dist2 * Math.sin(t2) - height2 / 2 + windowHeight2 / 2);
+//   mercury.css('left', dist2 * Math.cos(t2) - width2 / 2 + windowWidth2 / 2);
+//   t2 += 0.015;
+// };
+//
+// var blue1 = $('<img class="blue1" style="width:100px" src="img/5.png" />');
+//
+// $('.foreground2').append(blue1);
+//
+// var t3 = 0;
+// var dist3 = 600;
+// var drawBlue1 = function() {
+//   var width3 = blue1.width();
+//   var height3 = blue1.height();
+//   var windowWidth3 = $(window).width();
+//   var windowHeight3 = $(window).height();
+//   blue1.css('top', dist3 * Math.sin(t3) - height3 / 2 + windowHeight3 / 2);
+//   blue1.css('left', dist3 * Math.cos(t3) - width3 / 2 + windowWidth3 / 2);
+//   t3 += 0.007;
+// };
+//
+//
+// // var moon = $('<img class="moon" style="width:10px" src="img/6.png" />');
+// //
+// // $('.foreground3').append(moon);
+// //
+// // var t4 = 0;
+// // var dist4 = 100;
+// // var drawMoon = function() {
+// //   var earthWidth = earth.width();
+// //   var earthHeight = earth.height();
+// //   var width4 = moon.width();
+// //   var height4 = moon.height();
+// //   moon.css('top', dist4 * Math.sin(t4) - height4 / 2 + earthHeight / 2);
+// //   moon.css('left', dist4 * Math.cos(t4) - width4 / 2 + earthWidth / 2);
+// //   t4 += 0.015;
+// // };
+// //
+// // setInterval(drawMoon, 20);
+// setInterval(drawBlue1, 20);
+// setInterval(drawEarth, 20);
+// setInterval(drawMercury, 20);
